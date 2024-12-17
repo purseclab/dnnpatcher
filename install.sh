d5d8067149d71342e4d55283fb38bc2a135c7499
sam41290 <soumya41290@gmail.com>
2024-12-17 03:31:33 +0000

    Added code and install script

---
+++ install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+git clone https://github.com/purseclab/DnD.git
+python3 -m venv patcher
+source patcher/bin/activate
+pip install -r ./DnD/angr_env/requirements.txt
+cp DnD/angr_env/base.py patcher/lib/python3.8/site-packages/claripy/ast/base.py
+pip install -U patcherex2
+deactivate
